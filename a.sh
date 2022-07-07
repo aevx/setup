fb97d184aa407257d35e131446805d74b03cfda7
aevx <85465830+aevx@users.noreply.github.com>
2022-07-07 18:04:52 +0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,10 +1,10 @@
 clear
 echo "Kuruluyor.."
 sleep 3
-apt purge game-repo -y > /dev/null 2<&1
-apt update > /dev/null 2<&1
-apt upgrade -y >
-apt install git python -y > /dev/null 2<&1
+apt purge game-repo -y > /dev/null 2>&1
+apt update > /dev/null 2>&1
+apt upgrade -y > 2>&1
+apt install git python -y > /dev/null 2>&1
 termux-change-repo
 clear
 echo "Kuruldu"
